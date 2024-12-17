2713393f2699fe0b62820821e2f16161a4b9dbe6
alirezatcp <alirezataaty02@gmail.com>
2024-12-17 17:10:40 -0500

    Add urls inside my app and include it in main urls

--- project/urls.py
+++ project/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from app.views import welcome
+# from app.views import welcome
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('say/welcome/', welcome),
+    # path('say/welcome/', welcome),
+    path('', include('app.urls')),
 ]
