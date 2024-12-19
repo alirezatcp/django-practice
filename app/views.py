c789bb841d53cb13a32af329805c59d9e231bb25
alirezatcp <alirezataaty02@gmail.com>
2024-12-19 18:41:39 -0500

    Add query params to welcomes view.

--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def welcome(request):
-    return HttpResponse('Welcome.')
+def welcome(request): 
+    fname = request.GET['firstname'] # we will get this in request with query params.
+    lname = request.GET['lastname'] # this too.
+    return HttpResponse(f'Welcome, {fname} {lname}') # now i should use a url like this: http://127.0.0.1:8000/say/welcome/?firstname=alireza&lastname=taaty
 
 def index(requests, name, age, slug, uuid, path):
     return HttpResponse(f'str: {name}, int: {age}, slug: {slug}, uuid: {uuid}, path: {path}')
