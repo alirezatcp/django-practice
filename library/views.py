87c73736e8674dcbac56282843739799f52246a3
alirezatcp <alirezataaty02@gmail.com>
2024-12-21 11:37:53 -0500

    Add book_detail template to library app.

--- library/views.py
+++ library/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 from library.models import Book
 
 def book_detail(request, book_id):
-    book = Book.objects.get(id=book_id)
+    book = Book.objects.get(pk=book_id)
     context = {'book': book}
     return render(request, 'book_detail.html', context=context)
