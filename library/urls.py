a42e0b528efc0da1ea3c8372c32392a4243900d7
alirezatcp <alirezataaty02@gmail.com>
2024-12-22 06:27:48 -0500

    Add a view to list books.

--- library/urls.py
+++ library/urls.py
@@ -4,4 +4,5 @@ from library import views
 
 urlpatterns = [
     path('books/<int:book_id>/', views.book_detail, name='book_detail'),
+    path('books/', views.books_list, name='books_list'),
 ]
--- library/views.py
+++ library/views.py
@@ -6,3 +6,8 @@ def book_detail(request, book_id):
     book = Book.objects.get(pk=book_id)
     context = {'book': book}
     return render(request, 'book_detail.html', context=context)
+
+def books_list(request):
+    books = Book.objects.all()
+    context = {'books': books}
+    return render(request, 'books_list.html', context=context)
