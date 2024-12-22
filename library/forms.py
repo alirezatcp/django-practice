45f918ac43556d2d7cae5b2853b16fe58759d1e8
alirezatcp <alirezataaty02@gmail.com>
2024-12-22 17:05:53 -0500

    Add a form to library.

---
+++ library/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+class BookForm(forms.Form):
+    author = forms.CharField()
+    title = forms.CharField()
+    email = forms.EmailField(required=False)
+
+    # if we want to add filter to one field we should use this structure to function name: clean_<field_name>
+    # add a filter to author that it length should be bigger than 3 chars.
+    def clean_author(self):
+        author = self.cleaned_data['author']
+        if len(author) < 3:
+            message = 'author name should be bigger than 3 characters.'
+            raise forms.ValidationError(message)
+
+        return author 
