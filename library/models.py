4f0f05222d480b8f8a94d97e046bdcaf57d34b52
alirezatcp <alirezataaty02@gmail.com>
2024-12-22 17:23:05 -0500

    Add a POST view to library using form with httpresponse.

--- library/models.py
+++ library/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Book(models.Model):
     author = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.author} - {self.title}'
--- library/views.py
+++ library/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from library.models import Book
+
+from library.forms import BookForm
 
 def book_detail(request, book_id):
     book = Book.objects.get(pk=book_id)
@@ -11,3 +14,19 @@ def books_list(request):
     books = Book.objects.all()
     context = {'books': books}
     return render(request, 'books_list.html', context=context)
+
+def add_book(request):
+    if request.method == 'GET':
+        books = Book.objects.all()
+        context = {'books': books}
+        return render(request, 'books_list.html', context=context)
+
+    elif request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            book = Book(author=data['author'], title=data['title'])
+            book.save()
+            return HttpResponse(book, status=201)
+
+        return HttpResponse('Error', status=400)
