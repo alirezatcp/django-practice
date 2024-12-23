9245d14f9c02b48d106926b5d0bb4a33e25134b1
alirezatcp <alirezataaty02@gmail.com>
2024-12-23 13:41:56 -0500

    Add list view with generic and fix MyView vision.

--- cbv/urls.py
+++ cbv/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from cbv.views import MyView
+from cbv.views import MyView, UserListView
 
 urlpatterns = [
-    path('cbv/', MyView.as_view())
+    path('cbv/', MyView.as_view()),
+    path('list_view/', UserListView.as_view())
 ]
--- cbv/views.py
+++ cbv/views.py
@@ -4,8 +4,19 @@ from django.views import View
 
 from cbv.models import User
 
+from django.views.generic import ListView
+
 class MyView(View):
     def get(self, request):
         users = User.objects.all()
         users_list = [user.name for user in users] # add users objects to a list to show. if we had __str__ in model we could use users itself.
-        return HttpResponse(users_list)
+        show = ' '.join(users_list)
+        return HttpResponse(show)
+
+class UserListView(ListView):
+    model = User # if we want to filter we can use this instead this line or use get_queryset function (commented in below): queryset = User.objects.filter(name='alireza')
+    template_name = 'list.html' # send object to template with name: object_list. default template_name is 'User_list.html'
+    paginate_by = 3 # shows every 3 object in a page. we will see every page with this query param (for example page 1): ?page=1
+
+    # def get_queryset(self):
+    #     return User.objects.filter(name='alireza')
