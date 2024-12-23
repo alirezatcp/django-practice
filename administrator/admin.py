dd0a77b8de175819cb0056d14b72f10429cdb8a7
alirezatcp <alirezataaty02@gmail.com>
2024-12-23 16:40:47 -0500

    Add a model to administrator app and register in admin and create a superuser.

--- administrator/admin.py
+++ administrator/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from administrator.models import Author, Book
+
+# created a superuser with this command: python manage.py createsuperuser
+# alireza, 123, a@gmail.com
+
+admin.site.register(Book)
+admin.site.register(Author)
+
--- administrator/models.py
+++ administrator/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.DateField(blank=True, null=True)
+    birth_date = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    public_date = models.DateField()
+    pages_count = models.IntegerField()
+    aauthor = models.ForeignKey(Author, on_delete=models.CASCADE)
