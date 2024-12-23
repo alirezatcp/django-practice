5a940a3f1fa0693651411fed8d4f6967dbc3ad62
alirezatcp <alirezataaty02@gmail.com>
2024-12-23 14:03:05 -0500

    Add detail view with generic.

--- cbv/urls.py
+++ cbv/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from cbv.views import MyView, UserListView
+from cbv.views import MyView, UserListView, UserDetailView
 
 urlpatterns = [
     path('cbv/', MyView.as_view()),
-    path('list_view/', UserListView.as_view())
+    path('list_view/', UserListView.as_view()),
+    path('detail_view/<int:pk>', UserDetailView.as_view())
 ]
--- cbv/views.py
+++ cbv/views.py
@@ -4,7 +4,7 @@ from django.views import View
 
 from cbv.models import User
 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 class MyView(View):
     def get(self, request):
@@ -15,8 +15,12 @@ class MyView(View):
 
 class UserListView(ListView):
     model = User # if we want to filter we can use this instead this line or use get_queryset function (commented in below): queryset = User.objects.filter(name='alireza')
-    template_name = 'list.html' # send object to template with name: object_list. default template_name is 'User_list.html'
+    template_name = 'list.html' # send object to template with name: object_list. default template_name is 'cvb/User_list.html'
     paginate_by = 3 # shows every 3 object in a page. we will see every page with this query param (for example page 1): ?page=1
 
     # def get_queryset(self):
     #     return User.objects.filter(name='alireza')
+
+class UserDetailView(DetailView):
+    model = User # we can filter like ListView here too.
+    template_name = 'detail.html' # send object to template with name: user. default template_name is 'cvb/User_detail.html'
